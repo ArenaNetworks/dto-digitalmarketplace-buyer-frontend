cd84655126bd1e424c61392e6b5b27746e0714c1
anthonymckerron <47020580+anthonymckerron@users.noreply.github.com>
2019-07-10 10:38:25 +1000

    Changing contact-us hrefs to point to zendesk article (#831)
    * Changing hrefs to point to zendesk article
    
    * Removing physical locations from contact-us page.
    
    * Adding redirect for contact-us
    
    * Removing contact-us page as no longer required.

--- app/main/views/marketplace.py
+++ app/main/views/marketplace.py
@@ -66,6 +66,9 @@ def content(template_name):
     if template_name == 'capabilities-and-rates':
         return redirect('https://marketplace1.zendesk.com/hc/en-gb/articles/360000080555-Daily-rates-trend-charts',
                         code=301)
+    if template_name == 'contact-us':
+        return redirect('https://marketplace1.zendesk.com/hc/en-gb/articles/360001050936',
+                        code=301)
     try:
         return render_template('content/{}.html'.format(template_name))
     except:  # noqa
